7534326266b519a8c2d565aa20aca16d60d21841
dineshsabbisetti <dineshsabbisetti@users.noreply.github.com>
2022-04-13 12:51:18 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+
+console.log("hello world");
+console.log("hello world");
+let person = {
+    name: 'dinesh',
+    age: 23
+};
+
+person.name = 'superdinesh';
+
+person['name'] = 'dinesh';
+
+console.log(person);
